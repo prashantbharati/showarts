9e3d407d64ee44992be2e38aea15d1db5579ff97
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-02-05 21:45:05 +0530

    routes folder

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import postRoutes from "./posts.js";
+import postRoutes from "./routes/posts.js";
 const app = express();
 
 dotenv.config();
