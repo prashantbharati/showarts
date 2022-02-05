0d06eec060442e1fdf297a6f80945ed81639ab95
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-02-05 20:31:51 +0530

    added databse

--- server/index.js
+++ server/index.js
@@ -3,3 +3,17 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+
+const PORT = 5000;
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port:${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
