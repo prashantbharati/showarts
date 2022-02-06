95b278de0621e7911fee464fad9940e478022cfe
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-02-06 08:03:05 +0530

    get route done

--- src/components/List.js
+++ src/components/List.js
@@ -1,36 +1,10 @@
 import React from "react";
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
-// export const List = async ({ posts }) => {
-//   const url = "http://localhost:5000/posts";
-//   const npost = { posts };
-
-//   posts.map(async (post) => {
-//     console.log(post);
-//     // await axios.post(url, post);
-//   });
-
-//   // const { data } = await axios.post(url, npost);
-
-//   console.log(posts);
-//   return (
-//     <div>
-//       {posts.map((post) => (
-//         <div>
-//           <p>{post.fullname}</p>
-//           <p>{post.username}</p>
-//           <p>{post.email}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
 const api = axios.create({ baseURL: `http://localhost:5000/posts` });
 
-export const List = ({ posts }) => {
-  console.log(posts);
+export const List = async ({ posts }) => {
+  // console.log(posts);
   posts.map(async (post) => {
     const { data } = await api.post("/", {
       fullname: post.fullname,
@@ -43,6 +17,14 @@ export const List = ({ posts }) => {
       console.log(error);
     }
   });
+
+  const data = await api.get("/");
+  try {
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <div>
       {posts.map((post) => (
