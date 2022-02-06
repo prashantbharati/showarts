d94e89623a05e0d99c3db0245ae4ab5591d4ac42
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-02-06 13:56:59 +0530

    making it neat

--- src/components/table.js
+++ src/components/table.js
@@ -7,18 +7,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-//   createData("Eclair", 262, 16.0, 24, 6.0),
-//   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9),
-// ];
-
 export default function BasicTable({ data1 }) {
   return (
     <TableContainer component={Paper}>
@@ -44,17 +32,12 @@ export default function BasicTable({ data1 }) {
         </TableHead>
         <TableBody>
           {data1.map((row) => (
-            <TableRow
-            //   key={row.fullname}
-            //   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
+            <TableRow>
               <TableCell component="th" scope="row">
                 {row.fullname}
               </TableCell>
               <TableCell align="right">{row.username}</TableCell>
               <TableCell align="right">{row.email}</TableCell>
-              {/* <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell> */}
             </TableRow>
           ))}
         </TableBody>
